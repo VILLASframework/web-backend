d5fb9047727091aac3d3aff5c655b3c5827295cb
smavros <stemavros@gmail.com>
2019-04-29 00:00:49 +0200

    Adds gin-gonic and initial routing

--- start.go
+++ start.go
@@ -2,33 +2,19 @@ package main
 
 import (
 	"git.rwth-aachen.de/acs/public/villas/villasweb-backend-go/common"
+	"gopkg.in/gin-gonic/gin.v1"
 )
 
 func main() {
 	// Testing
 	db := common.InitDB()
+	common.MigrateModels(db)
 	defer db.Close()
 
-	//Testing dependencies
-	var testProject common.Project
-	testProject.Name = "MyAweSomeProject"
+	r := gin.Default()
 
-	var testFile common.File
-	testFile.Name = "MyAwesomeFilename"
+	api := r.Group("/api")
+	common.UsersRegister(api.Group("/users"))
 
-	var testSimulation common.Simulation
-	testSimulation.Name = "MyAwesomeSimulation"
-
-	var testSimulationModel common.SimulationModel
-	testSimulationModel.Name = "MyAwesomeSimulationModel"
-
-	var testSimulator common.Simulator
-	testSimulator.Host = "SimulatorHost"
-
-	var testUser common.User
-	testUser.Username = "MyUserName"
-
-	var testVis common.Visualization
-	testVis.Name = "MyAwesomeVisualization"
-
+	r.Run()
 }
