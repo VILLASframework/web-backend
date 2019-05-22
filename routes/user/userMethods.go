6289b859bd0c096224b8317a16030547c2bfd4b1
smavros <stemavros@gmail.com>
2019-05-22 11:20:29 +0200

    Implements user.SetPassword() method

--- routes/user/userMethods.go
+++ routes/user/userMethods.go
@@ -1,9 +1,12 @@
 package user
 
 import (
+	"fmt"
 	"git.rwth-aachen.de/acs/public/villas/villasweb-backend-go/common"
 	"golang.org/x/crypto/bcrypt"
 )
+
+const bcryptCost = 10
 
 // TODO: use validator
 type Credentials struct {
@@ -17,24 +20,32 @@ type Credentials struct {
 // package since we have circular dependencies. Methods of a type must
 // live in the same package.
 type User struct {
-	Model common.User
+	common.User // check golang embedding types
 }
 
 func FindUserByUsername(username string) (User, error) {
 	db := common.GetDB()
 	var user User
-	err := db.Find(&user.Model, "Username = ?", username).Error
+	err := db.Find(&user, "Username = ?", username).Error
 	return user, err
 }
 
-func (u *User) setPassword(password string) error {
-	// TODO: Not implemented
+func (u *User) SetPassword(password string) error {
+	if len(password) == 0 {
+		return fmt.Errorf("Password cannot be empty")
+	}
+	newPassword, err :=
+		bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
+	if err != nil {
+		return fmt.Errorf("Failed to generate hash from password")
+	}
+	u.Password = string(newPassword)
 	return nil
 }
 
 func (u *User) validatePassword(password string) error {
 	loginPassword := []byte(password)
-	hashedPassword := []byte(u.Model.Password)
+	hashedPassword := []byte(u.Password)
 	return bcrypt.CompareHashAndPassword(hashedPassword, loginPassword)
 }
 
