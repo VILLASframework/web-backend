b100d47d68960a1e836a6b63e474e32fe9fb7f1b
Sonja Happ <sonja.happ@eonerc.rwth-aachen.de>
2019-06-03 12:49:57 +0200

    WIP add authentication to testing

--- routes/simulation/simulation_test.go
+++ routes/simulation/simulation_test.go
@@ -1,8 +1,10 @@
 package simulation
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"git.rwth-aachen.de/acs/public/villas/villasweb-backend-go/routes/user"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,6 +14,19 @@ import (
 
 	"git.rwth-aachen.de/acs/public/villas/villasweb-backend-go/common"
 )
+
+
+var token string
+
+type credentials struct{
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var cred = credentials{
+	Username: "User_A",
+	Password: "abc123",
+}
 
 var msgOK = common.ResponseMsg{
 	Message: "OK.",
@@ -72,7 +87,16 @@ func TestSimulationEndpoints(t *testing.T) {
 
 	router := gin.Default()
 	api := router.Group("/api")
+
+	// All endpoints require authentication except when someone wants to
+	// login (POST /authenticate)
+	user.VisitorAuthenticate(api.Group("/authenticate"))
+
+	api.Use(user.Authentication(true))
+
 	RegisterSimulationEndpoints(api.Group("/simulations"))
+
+	credjson, err := json.Marshal(cred)
 
 	msgOKjson, err := json.Marshal(msgOK)
 	if err != nil {
@@ -95,30 +119,69 @@ func TestSimulationEndpoints(t *testing.T) {
 	}
 
 	// TODO add authentication for User_A
+	authenticate(t, router, "/api/authenticate", "POST", credjson, 200)
 
 	// test GET simulations/
-	testEndpoint(t, router, "/api/simulations/", "GET", "", 200, string(msgSimulationsjson))
+	testEndpoint(t, router, "/api/simulations/", "GET", nil, 200, string(msgSimulationsjson))
 
 	// test GET simulations/:SimulationID
-	testEndpoint(t, router, "/api/simulations/1", "GET", "", 200, string(msgSimulationjson))
+	testEndpoint(t, router, "/api/simulations/1", "GET", nil, 200, string(msgSimulationjson))
 
 	// test GET simulations/:SimulationID/users
-	testEndpoint(t, router, "/api/simulations/1/users", "GET", "", 200, string(msgUsersjson))
+	testEndpoint(t, router, "/api/simulations/1/users", "GET", nil, 200, string(msgUsersjson))
 
 	// test DELETE simulations/:SimulationID/user
-	testEndpoint(t, router, "/api/simulations/1/user?username=User_A", "DELETE", "", 200, string(msgOKjson))
+	testEndpoint(t, router, "/api/simulations/1/user?username=User_A", "DELETE", nil, 200, string(msgOKjson))
 
 	// test PUT simulations/:SimulationID/user
-	testEndpoint(t, router, "/api/simulations/1/user?username=User_A", "PUT", "", 200, string(msgOKjson))
+	testEndpoint(t, router, "/api/simulations/1/user?username=User_A", "PUT", nil, 200, string(msgOKjson))
 
 	// TODO add more tests
 }
 
-func testEndpoint(t *testing.T, router *gin.Engine, url string, method string, body string, expected_code int, expected_response string) {
+func testEndpoint(t *testing.T, router *gin.Engine, url string, method string, body []byte, expected_code int, expected_response string) {
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(method, url, nil)
-	router.ServeHTTP(w, req)
+
+
+	if body != nil{
+		req, _ := http.NewRequest(method, url, bytes.NewBuffer(body))
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Add("Authorization", "TOKEN " + token)
+		router.ServeHTTP(w, req)
+	} else {
+		req, _ := http.NewRequest(method, url, nil)
+		req.Header.Add("Authorization", "TOKEN " + token)
+		router.ServeHTTP(w, req)
+	}
+
 	assert.Equal(t, expected_code, w.Code)
 	fmt.Println(w.Body.String())
 	assert.Equal(t, expected_response, w.Body.String())
+}
+
+func authenticate(t *testing.T, router *gin.Engine, url string, method string, body []byte, expected_code int) {
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest(method, url, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, expected_code, w.Code)
+
+	var body_data map[string]interface{}
+
+	err := json.Unmarshal([]byte(w.Body.String()), &body_data)
+	if err != nil{
+		panic(err)
+	}
+
+
+	success := body_data["success"].(bool)
+	if !success{
+		panic(-1)
+	}
+	// save token to global variable
+	token = body_data["token"].(string)
+
+	fmt.Println(w.Body.String())
 }
