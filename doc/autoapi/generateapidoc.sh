ea974ba32bf1daaec8eab4fce50e75e86e82d1cb
Sonja Happ <sonja.happ@eonerc.rwth-aachen.de>
2019-06-06 09:57:40 +0200

    generate and deploy api docs based on swag tool

--- doc/autoapi/generateapidoc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-cd ../../
-go mod tidy
-swag init -p pascalcase -g "start.go" -o "./doc/autoapi/"
-cd -
-
-redoc-cli bundle --cdn --title "VILLASweb Backend API" --output index.html swagger.yaml
