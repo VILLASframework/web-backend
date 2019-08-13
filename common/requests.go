487810f148e601f51b75dfae7818033d2d92bd0a
smavros <stemavros@gmail.com>
2019-08-13 19:15:31 +0200

    Adds Request type in requests.go:
        This struct will be the basis every possible request. For now it
        contains only the fields of the User request. The idea is that it
        will containt every possible field of any model together with the
        `omitempty` flag so we can marshal only the initialized fields. This
        will deprecate all the ResponseMsg* types that were actually used
        for requests.

--- common/requests.go
+++ common/requests.go
@@ -1,3 +1,10 @@
 package common
 
 type KeyModels map[string]interface{}
+
+type Request struct {
+	Username string `json:"username,omitemtpy"`
+	Password string `json:"password,omitempty"`
+	Mail     string `json:"mail,omitempty"`
+	Role     string `json:"role,omitempty"`
+}
