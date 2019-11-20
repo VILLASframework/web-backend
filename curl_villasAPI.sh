92f22035c62748619255014e5419d1d24c9f876f
Sonja Happ <sonja.happ@eonerc.rwth-aachen.de>
2019-11-20 11:08:33 +0100

    add GPL 3.0 license statement in all (not auto-generated) code files

--- curl_villasAPI.sh
+++ curl_villasAPI.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-
-# Author: Stefanos Mavros 2019
+##########################################################################################
+# Bash script for testing with curl.
 #
+# @author Stefanos Mavros <stefanos.mavros@rwth-aachen.de>
+# @copyright 2014-2019, Institute for Automation of Complex Power Systems, EONERC
+# @license GNU General Public License (version 3)
+#
+# VILLASweb-backend-go
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##########################################################################################
+
 # Handy bash script using curl for testing the checkpoint of the
 # VILLASweb frontend. See at the end of the file for usage cases
 
--- routes/simulator/simulator_validators.go
+++ routes/simulator/simulator_validators.go
@@ -1,3 +1,24 @@
+/** Simulator package, validators.
+*
+* @author Sonja Happ <sonja.happ@eonerc.rwth-aachen.de>
+* @copyright 2014-2019, Institute for Automation of Complex Power Systems, EONERC
+* @license GNU General Public License (version 3)
+*
+* VILLASweb-backend-go
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************************/
 package simulator
 
 import (
